9c494a302f1d1027f34c02c2f7bcce2f2a33b6eb
Paweł Beder <pawel.beder@gmail.com>
2020-07-17 20:39:58 +0200

    feat: Hamburger menu for mobile layout

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-const name = 'Paweł'
-const age = 32
-console.log('Logged dupa')
+const name = "Paweł";
+const age = 32;
+console.log("Logged dupa");
+
+const hamburger = document.querySelector(".hamburger--js");
+console.log(hamburger);
+
+hamburger.addEventListener("click", () => {
+  const nav = document.querySelector(".navigation--js");
+  nav.classList.toggle("navigation__open");
+});
